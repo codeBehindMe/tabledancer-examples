841475b56e491ef76599d123524e02b271fcd321
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-25 09:58:53 +0000

    Added basic example

---
+++ .github/workflows/deploy_ddl.sh
@@ -0,0 +1,5 @@
+set -exo pipefail
+
+pip install git+https://github.com/codeBehindMe/tabledancer.git --user --install-option="--style=databricks8.1"
+
+tabledancer dance ./ddl/my_table.yaml
---
+++ main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+
+  print("Hello from tabledancer example. This is not a real project!")
