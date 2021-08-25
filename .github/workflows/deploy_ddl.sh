b321c1c5402dd4c7c1272246e0ea5a8f474c094d
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-25 10:06:17 +0000

    Added missing databricks config workaround

--- .github/workflows/deploy_ddl.sh
+++ .github/workflows/deploy_ddl.sh
@@ -4,4 +4,6 @@ set -exo pipefail
 
 pip install git+https://github.com/codeBehindMe/tabledancer.git --user --install-option="--style=databricks8.1"
 
+echo {\"host\":\""$DATABRICKS_ADDRESS"\", \"token\":\"$DATABRICKS_API_TOKEN\", \"cluster_id\":\"$DATABRICKS_CLUSTER_ID\",\"org_id\":\"0\",\"port\":\"15001\"} > $HOME/.databricks-connect
+
 tabledancer dance ./ddl/my_table.yaml
