4db377d4bdd58f46718b789dc6cbe49939ffd542
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-25 10:18:48 +0000

    Updated install from patch

--- .github/workflows/deploy_ddl.sh
+++ .github/workflows/deploy_ddl.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-pip install git+https://github.com/codeBehindMe/tabledancer.git --user --install-option="--style=databricks8.1"
+pip install git+https://github.com/codeBehindMe/tabledancer.git@4f883d1e04b67eb1bb57b1192fd4b92e510c72a0 --user --install-option="--style=databricks8.1"
 
 echo {\"host\":\""$DATABRICKS_ADDRESS"\", \"token\":\"$DATABRICKS_API_TOKEN\", \"cluster_id\":\"$DATABRICKS_CLUSTER_ID\",\"org_id\":\"0\",\"port\":\"15001\"} > $HOME/.databricks-connect
 
