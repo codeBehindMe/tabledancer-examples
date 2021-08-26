fac6cea8b0f59b948dca032cbe95ac854fb99c94
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-26 00:50:45 +0000

    Fixed incorrect space

--- .github/workflows/deploy_ddl.sh
+++ .github/workflows/deploy_ddl.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-pip install git+git://github.com/codeBehindMe/tabledancer.git--install-option="--style=databricks8.1"
+pip install git+git://github.com/codeBehindMe/tabledancer.git --install-option="--style=databricks8.1"
 
 echo {\"host\":\""$DATABRICKS_ADDRESS"\", \"token\":\"$DATABRICKS_API_TOKEN\", \"cluster_id\":\"$DATABRICKS_CLUSTER_ID\",\"org_id\":\"0\",\"port\":\"15001\"} > $HOME/.databricks-connect
 
